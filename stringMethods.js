da31bb466136e1b57d4e79bf92373510bb5fb44a
Saima Eva <69007708+Saima-Eva@users.noreply.github.com>
2021-03-18 22:56:07 +0600

    Add files via upload

---
+++ stringMethods.js
@@ -0,0 +1,16 @@
+
+var a = 'I am'
+var b = 'Saima Eva'
+
+var c = a.concat(' ',b)
+var d = c.substr(11)
+console.log(c)
+console.log(d)
+console.log(c.charAt(5))
+console.log(c.startsWith('I am'))
+console.log(c.endsWith('Saima'))
+
+console.log(a.toUpperCase())
+console.log(b.toLowerCase())
+console.log('          Saima       '.trim())
+console.log(c.split(' '))
