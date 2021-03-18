5c6871f55e2850cf693abf16321200ba8835d966
Saima Eva <69007708+Saima-Eva@users.noreply.github.com>
2021-03-18 19:21:39 +0600

    Add files via upload

--- string.js
+++ string.js
@@ -9,3 +9,18 @@ var n =10
 var str = String(n)
 console.log(str)
 */
+
+
+//**** Escape Notation
+
+var str = 'This is \'String\''
+console.log(str)
+
+var str =' Saima Yesmin Eva \n Sumiya Akter Toma'
+console.log(str)
+
+var str = 'Saima Yesmin \t Eva'
+console.log(str)
+
+var str = 'Saima \\ Yesmin \\ Eva'
+console.log(str)
