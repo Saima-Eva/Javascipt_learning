d8f23b6e8dba2dc8aa1598be0737607e650b6e7c
Saima Eva <69007708+Saima-Eva@users.noreply.github.com>
2021-03-19 13:50:49 +0600

    Add files via upload

---
+++ stringLength.js
@@ -0,0 +1,19 @@
+
+var str = 'Some String'
+
+var length = 0
+
+while(true){
+    if(str.charAt(length) === ''){
+    break
+}
+else{
+    length++
+  }
+
+}
+console.log(length)
+
+
+var a = 'Saima'
+console.log(a.length)
