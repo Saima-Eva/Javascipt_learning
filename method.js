d331e95b9b72085b92c79c3a5366ac5fbeea9e6d
Saima Eva <69007708+Saima-Eva@users.noreply.github.com>
2021-04-04 00:33:12 +0600

    Add files via upload

---
+++ method.js
@@ -0,0 +1,12 @@
+var arr = [4, 5, 9, 6]
+var arr2 = [5, 10, 45, 23]
+
+console.log(arr.join(' '))
+console.log(arr.join(' ,'))
+console.log(arr.join(' | '))
+
+// arr.fill(0)
+// console.log(arr)
+
+var arr3 = arr.concat(arr2)
+console.log(arr3)
